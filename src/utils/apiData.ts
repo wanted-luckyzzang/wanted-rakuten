14e661c3d59e93d04ee2f0634242825515568d69
안병진 <byungjin0120@gmail.com>
2022-02-25 04:11:55 +0900

    feat: util함수 추가

--- src/utils/apiData.ts
+++ src/utils/apiData.ts
@@ -1,13 +1,15 @@
 import { ApiDataType, DataType } from "types";
 
-export const getLastetCreatedAt = (data: ApiDataType): number => {
-  let max = data[0].created_at;
+export const getLatestCreatedAt = (data: ApiDataType | undefined): number => {
+  if (data) {
+    let max = data[0].created_at;
+    for (let idx = 1; idx < data.length; idx++) {
+      if (data[idx].created_at > max) max = data[idx].created_at;
+    }
 
-  for (let idx = 1; idx < data.length; idx++) {
-    if (data[idx].created_at > max) max = data[idx].created_at;
+    return max;
   }
-
-  return max;
+  return 0;
 };
 
 export const getKeyFilterData = (
