8d1e5da15b842188e9b41aaad3521384bddfb674
jiyong LEE <49055628+Jiyong95@users.noreply.github.com>
2022-02-25 05:29:08 +0900

    feat: clipboard 복사 기능 추가

---
+++ src/utils/clipboard.ts
@@ -0,0 +1,18 @@
+const clipboard = (text: string) => {
+  if (!document.queryCommandSupported("copy")) {
+    return alert("복사하기가 지원되지 않는 브라우저입니다.");
+  }
+  const textarea = document.createElement("textarea");
+  textarea.value = text;
+  textarea.style.top = "0";
+  textarea.style.left = "0";
+  textarea.style.position = "fixed";
+  document.body.appendChild(textarea);
+  textarea.focus();
+  textarea.select();
+  document.execCommand("copy");
+  document.body.removeChild(textarea);
+  alert(`${text} 주소가 복사되었습니다.`);
+};
+
+export default clipboard;
