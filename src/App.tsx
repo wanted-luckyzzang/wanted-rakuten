790e6b06a6de61459e9c18dce53a865b1dbf54fa
jiyong LEE <49055628+Jiyong95@users.noreply.github.com>
2022-02-24 20:42:35 +0900

    feat: API 데이터 받아오기

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,21 @@
+import React, { useEffect, useState } from "react";
+import { Route, Routes } from "react-router";
+import { BrowserRouter } from "react-router-dom";
 import Container from "components/Container";
 import DetailPage from "pages/DetailPage";
 import LinkPage from "pages/LinkPage";
 import GlobalStyle from "styles/GlobalStyle";
-import { BrowserRouter } from "react-router-dom";
-import { Route, Routes } from "react-router";
+import axios from "axios";
 
 function App() {
+  const [data, setData] = useState();
+  useEffect(() => {
+    (async () => {
+      const { data } = await axios.get("homeworks/links");
+      setData(data);
+    })();
+  }, []);
+  console.log(data);
   return (
     <BrowserRouter>
       <GlobalStyle />
