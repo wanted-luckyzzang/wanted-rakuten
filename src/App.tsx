7d6bd6f086e7c77823a362eb5655de63bc747c15
Ahn <81910935+BYEONGJIN-AHN@users.noreply.github.com>
2022-02-25 17:59:16 +0900

    depoly: 배포 시도

--- src/App.tsx
+++ src/App.tsx
@@ -26,7 +26,7 @@ function App() {
       (async () => {
         try {
           const { data } = await axios.get(
-            "https:/storage-fe.fastraffic.io//homeworks/links"
+            "https:/storage-fe.fastraffic.io/homeworks/links"
           );
           setSession("data", data);
           setData(data);
